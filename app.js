2efbe849a90f6235516fe112a1de822259de348b
Brevan Bender <bcaybender@gmail.com>
2021-09-29 15:49:54 -0700

    All media is on the page about to start stretch work

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 // import functions and grab DOM elements
+const horseImg = document.getElementById('horse')
+const horseSound = document.getElementById('horsenoise')
+const dogImg = document.getElementById('dog')
+const dogSound = document.getElementById('dognoise')
+const catImg = document.getElementById('cat')
+const catSound = document.getElementById('catnoise')
+
 
 // initialize global state
+
+
+
+
+
+horseImg.addEventListener('click', ()=>
+
+    horseSound.play()
+
+)
+
+dogImg.addEventListener('click', ()=>
+
+    dogSound.play()
+)
+
+catImg.addEventListener('click', ()=>
+
+   catSound.play()
+)
 
 // set event listeners 
   // get user input
